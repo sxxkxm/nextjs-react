985f27f22d3140f3c21986d491f8b1701a977067
rntkatn01 <rntkatn01@outlook.com>
2023-05-06 18:28:14 -0400

    cart page import

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,32 @@
+import {age, name} from './data.js'
+
 export default function Cart() {
   return (
     <div>
       <h4 className="title">Cart</h4>
       <div className="cart-item">
         <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
+        <p>$40 {name}</p>
+        <p>1개 {age}</p>
       </div>
       <div className="cart-item">
         <p>상품명</p>
         <p>$40</p>
         <p>1개</p>
       </div>
+      <CartItem/>
+      <CartItem/>
+      <CartItem/>
     </div>
+  )
+}
+
+function CartItem(){
+  return (
+    <div className="cart-item">
+      <p>상품명</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>    
   )
 }
