0035907e10a0fe631c5b9af3adfe28df900e9822
rntkatn01 <rntkatn01@outlook.com>
2023-05-08 23:56:27 -0400

    updated not to use app folder

--- app/cart/data.js
+++
@@ -1,3 +0,0 @@
-let age = 20;
-let name = 'kim';
-export {age, name};
--- app/register/page.js
+++
@@ -1,9 +0,0 @@
-import './register.css';
-
-export default function Register() {
-  return (
-    <div>
-      <h4 className="title">Login</h4>
-    </div>
-  )
-}
