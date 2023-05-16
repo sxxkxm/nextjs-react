f284a344069d4da9edf233cfa6f7c17690e9f47d
rntkatn01 <rntkatn01@outlook.com>
2023-05-15 23:57:48 -0400

    css

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,26 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+// import styles from "./NavBar.module.css"
 
 export default function Navbar(){
   const router = useRouter();
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+    <nav >
+      <Link href="/" className={router.pathname === "/" ? "active" : ""}>Home</Link>
+      <Link href="/about" className={router.pathname === "/about" ? "active" : ""}>About</Link>
+      <style jsx>{`
+          nav {
+            background-color:tomato;
+          }
+          a {
+            text-decoration: none;
+          }
+          .active {
+            color: blue;
+          }
+      `}</style>
+      {/* <Link href="/" className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</Link> */}
+      {/* <Link href="/about" className={`${styles.link} ${router.pathname === "/about" ? styles.active : ""}`}>About</Link> */}
     </nav>  
   )
 }
